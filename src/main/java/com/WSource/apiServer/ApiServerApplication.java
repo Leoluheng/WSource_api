76bc2b1ef1b59f864774c4e88241069bd4af9c8e
LeoLuheng <luheng.leo@gmail.com>
2021-06-06 17:04:58 -0400

    Setup backend server skeleton using Spring Boot & Maven as pacakge management tool

---
+++ src/main/java/com/WSource/apiServer/ApiServerApplication.java
@@ -0,0 +1,23 @@
+package com.WSource.apiServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+	// TODO: demo use only; delete later
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
+
---
+++ src/test/java/com/WSource/apiServer/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WSource.apiServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
