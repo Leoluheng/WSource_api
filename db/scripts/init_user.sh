b08d1c0a262b5e330a487f7bc8dc50c0b75ae354
RileyHYZ <ri.hyzhou@gmail.com>
2021-07-28 16:15:19 -0400

    fixed docker env variables not exported

--- db/scripts/init_user.sh
+++ db/scripts/init_user.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+echo '*****************************'
+echo $MYSQL_USER
+echo $MYSQL_ROOT_PASSWORD
+echo $MYSQL_ROOT_PASSWORD
+echo $MYSQL_PASSWORD
+echo '*****************************'
+
 set -e
 mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT SELECT, INSERT, DELETE, UPDATE ON wsourcedb.* TO '$MYSQL_USER'@'%';
+CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT SELECT, INSERT, DELETE, UPDATE, ALTER ON wsourcedb.* TO '$MYSQL_USER'@'localhost';
 EOSQL
