4fbd883a1e5f5110322b36963c4bd712309d6e9a
andyW <wang130903@gmail.com>
2021-08-03 16:33:56 +0200

    remove unused code

--- src/main/java/com/WSource/apiServer/ApiServerApplication.java
+++ src/main/java/com/WSource/apiServer/ApiServerApplication.java
@@ -17,14 +17,5 @@ public class ApiServerApplication {
 		SpringApplication.run(ApiServerApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
-//			}
-//		};
-//	}
 }
 
