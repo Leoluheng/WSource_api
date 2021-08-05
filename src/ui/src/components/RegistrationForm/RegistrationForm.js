a8c579b30a786a0226027e9e72417cf50c23eef2
RileyHYZ <ri.hyzhou@gmail.com>
2021-08-04 23:39:12 -0400

    updated registration page ui

--- src/ui/src/components/RegistrationForm/RegistrationForm.js
+++ src/ui/src/components/RegistrationForm/RegistrationForm.js
@@ -3,6 +3,19 @@ import axios from 'axios';
 import './RegistrationForm.css';
 import {ACCESS_TOKEN_NAME, API_BASE_URL} from '../../constants/apiConstants';
 import {withRouter} from "react-router-dom";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+import welcome from '../../assets/images/welcome.svg';
 
 function RegistrationForm(props) {
     const [state, setState] = useState({
@@ -26,7 +39,7 @@ function RegistrationForm(props) {
             const payload = {
                 "name": state.name,
                 "email": state.email,
-                "accessLevel": 1
+                "authority": 'ROLE_USER'
             }
             axios.post(API_BASE_URL + '/user/register', payload)
                 .then(function (response) {
@@ -66,70 +79,171 @@ function RegistrationForm(props) {
             props.showError('Passwords do not match');
         }
     }
-    return (
-        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
-            <form>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputName">Name</label>
-                    <input type="text"
-                           className="form-control"
-                           id="name"
-                           aria-describedby="user"
-                           placeholder="Enter Name"
-                           value={state.name}
-                           onChange={handleChange}
-                    />
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            padding: theme.spacing(10),
+            flexGrow: 1,
+        },
+        image: {
+            // backgroundImage: 'url(./../../assets/images/welcome.svg)',
+            // // backgroundImage: 'url(https://source.unsplash.com/random)',
+            // backgroundRepeat: 'no-repeat',
+            // backgroundSize: 'cover',
+            // backgroundPosition: 'center'
+        },
+        paper: {
+            margin: theme.spacing(5, 4),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            borderRadius: '35px',
+        },
+        avatar: {
+          margin: theme.spacing(1),
+          backgroundColor: '#ffbf00',
+        },
+        form: {
+          width: '100%', // Fix IE 11 issue.
+          marginTop: theme.spacing(2),
+        },
+        submit: {
+          margin: theme.spacing(3, 0, 2),
+          background: '#FFC100',
+          color: 'white',
+          borderRadius: 20,
+          height: '50px',
+          '&:hover': {
+            backgroundColor: 'white',
+            color: '#ffbf00',
+            },
+        },
+        img: {
+            margin: 'auto',
+            display: 'block',
+            maxWidth: '100%',
+            maxHeight: '100%',
+        },
+        link: {
+            color: "#AA8B2F",
+            "&:hover": {
+                color: "#E1B739",
+                textDecoration: "underline #ffbf00"
+            }
+        }
+    }))
+
+    const classes = useStyles();
+
+    return(
+        <Grid 
+            container 
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center" 
+            className={classes.root}
+            spacing={10}
+        >
+            <Grid item xs={12} sm={6} className={classes.image} spacing={10}>
+                <h1>Welcome to WSource</h1>
+                <p>Log in to get your special recommendations!</p>
+                <img className={classes.img} src={welcome}/>
+                <Link href="https://storyset.com/people" className={classes.link} variant="body2">People illustrations by Storyset</Link>
+            </Grid>
+            <Grid item xs={12} sm={6} component={Paper} elevation={6} spacing={10}>
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign Up
+                    </Typography>
+                    <form className={classes.form}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Name"
+                            name="name"
+                            autoComplete="name"
+                            autoFocus
+                            value={state.name}
+                            onChange={handleChange}
+                        />
+                        <div className="form-group text-left">
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                value={state.email}
+                                onChange={handleChange}
+                                aria-describedby="emailHelp"
+                            />
+                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <TextField
+                            variant="outlined"
+                            required
+                            margin="normal"
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={state.password}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            variant="outlined"
+                            required
+                            margin="normal"
+                            fullWidth
+                            name="confirmPassword"
+                            label="Confirm Password"
+                            type="password"
+                            id="confirmPassword"
+                            autoComplete="current-password"
+                            value={state.confirmPassword}
+                            onChange={handleChange} 
+                        />
+                         <FormControlLabel
+                            control={<Checkbox value="allowExtraEmails" color="secondary" />}
+                            label="I want to receive inspiration via email."
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            className={classes.submit}
+                            onClick={handleSubmitClick}
+                            >
+                            Sign Up
+                        </Button>
+                    </form>
+                    <div className="alert alert-success mt-2" style={{display: state.successMessage ? 'block' : 'none' }} role="alert">
+                        {state.successMessage}
+                    </div>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2" className={classes.link} onClick={() => redirectToLogin()}>
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email"
-                           className="form-control"
-                           id="email"
-                           aria-describedby="emailHelp"
-                           placeholder="Enter email"
-                           value={state.email}
-                           onChange={handleChange}
-                    />
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
-                        else.</small>
-                </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password"
-                           className="form-control"
-                           id="password"
-                           placeholder="Password"
-                           value={state.password}
-                           onChange={handleChange}
-                    />
-                </div>
-                <div className="form-group text-left">
-                    <label htmlFor="exampleInputPassword1">Confirm Password</label>
-                    <input type="password"
-                           className="form-control"
-                           id="confirmPassword"
-                           placeholder="Confirm Password"
-                           value={state.confirmPassword}
-                           onChange={handleChange}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="btn btn-primary"
-                    onClick={handleSubmitClick}
-                >
-                    Register
-                </button>
-            </form>
-            <div className="alert alert-success mt-2" style={{display: state.successMessage ? 'block' : 'none'}}
-                 role="alert">
-                {state.successMessage}
-            </div>
-            <div className="mt-2">
-                <span>Already have an account? </span>
-                <span className="loginText" onClick={() => redirectToLogin()}>Login here</span>
-            </div>
-        </div>
+            </Grid>
+            <Grid>
+
+            </Grid>
+        </Grid>
     )
 }
 
