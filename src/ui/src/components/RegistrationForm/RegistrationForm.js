cf891f2a15c1772753f37029354b8782a7356d0c
Riley Zhou <35350176+RileyHYZ@users.noreply.github.com>
2021-08-06 17:19:17 -0400

    Signup page UI (#9)
    * added compose volume to persist data
    
    * update readme
    
    * updated sign up profile form

--- src/ui/src/components/RegistrationForm/RegistrationForm.js
+++ src/ui/src/components/RegistrationForm/RegistrationForm.js
@@ -11,7 +11,11 @@ import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import NativeSelect from '@material-ui/core/NativeSelect';
 import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -25,7 +29,10 @@ function RegistrationForm(props) {
         email: "",
         password: "",
         confirmPassword: "",
-        successMessage: null
+        successMessage: null,
+        nextPage: false,
+        year: "",
+        program: ""
     })
     const handleChange = (e) => {
         const {id, value} = e.target
@@ -34,14 +41,33 @@ function RegistrationForm(props) {
             [id]: value
         }))
     }
+    const handleDropDownChange = (event) => {
+        const name = event.target.name;
+        setState({
+          ...state,
+          [name]: event.target.value,
+        });
+    }
+    const handlePageChange = () => {
+        if (state.email.length && state.password.length) {
+            setState(prevState => ({
+                ...prevState,
+                nextPage: !state.nextPage
+            }))
+        } else {
+            props.showError('Please enter valid username and password')
+        }
+    }
     const sendDetailsToServer = () => {
-        console.log("called!!!")
         if (state.email.length && state.password.length) {
             props.showError(null);
             const payload = {
                 "name": state.name,
                 "email": state.email,
-                "authority": 'ROLE_USER'
+                "authority": 'ROLE_USER',
+                // TODO: modify backend to connect
+                // "program": state.program,
+                // "year": state.year
             }
             axios.post(API_BASE_URL + '/user/register', payload)
                 .then(function (response) {
@@ -81,79 +107,14 @@ function RegistrationForm(props) {
             props.showError('Passwords do not match');
         }
     }
-
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            padding: theme.spacing(10),
-            flexGrow: 1,
-        },
-        paper: {
-            margin: theme.spacing(5, 4),
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            borderRadius: '35px',
-        },
-        avatar: {
-          margin: theme.spacing(1),
-          backgroundColor: '#ffbf00',
-        },
-        form: {
-          width: '100%', // Fix IE 11 issue.
-          marginTop: theme.spacing(2),
-        },
-        submit: {
-          margin: theme.spacing(3, 0, 2),
-          background: '#FFC100',
-          color: 'white',
-          borderRadius: 20,
-          height: '50px',
-          '&:hover': {
-            backgroundColor: 'white',
-            color: '#ffbf00',
-            },
-        },
-        img: {
-            margin: 'auto',
-            display: 'block',
-            maxWidth: '100%',
-            maxHeight: '100%',
-        },
-        link: {
-            color: "#AA8B2F",
-            "&:hover": {
-                color: "#E1B739",
-                textDecoration: "underline #ffbf00"
-            }
-        }
-    }))
-
-    const classes = useStyles();
-
-    return(
-        <Grid 
-            container 
-            direction="row"
-            justifyContent="space-around"
-            alignItems="center" 
-            className={classes.root}
-            spacing={10}
-        >
-            <Grid item xs={12} sm={6} spacing={10}>
-                <h1>Welcome to WSource</h1>
-                <p>Log in to get your special recommendations!</p>
-                <img className={classes.img} src={welcome}/>
-                <Link href="https://storyset.com/people" variant="body2" color="textSecondary">People illustrations by Storyset</Link>
-            </Grid>
-            <Grid item xs={12} sm={6} component={Paper} elevation={6} spacing={10}>
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
+    const SignUpForm = () => {
+        if (!state.nextPage) {
+            return (
+                <div>
+                    <Typography component="h1" variant="h5" align ="center">
                         Sign Up
                     </Typography>
-                    <form className={classes.form}>
+                    <form className={classes.form} >
                         <TextField
                             variant="outlined"
                             margin="normal"
@@ -218,11 +179,188 @@ function RegistrationForm(props) {
                             fullWidth
                             variant="contained"
                             className={classes.submit}
-                            onClick={handleSubmitClick}
+                            onClick={handlePageChange}
                             >
-                            Sign Up
+                            Continue
                         </Button>
                     </form>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <Typography component="h1" variant="h5" align="center">
+                        Complete Your Profile
+                    </Typography>
+                    <form className={classes.form} >
+                        <FormControl>
+                            <InputLabel htmlFor="year-native-helper">Year</InputLabel>
+                                <NativeSelect
+                                    value={state.year}
+                                    onChange={handleDropDownChange}
+                                    inputProps={{
+                                        name: 'year',
+                                        id: 'year-native-helper',
+                                    }}
+                                    >
+                                    <option aria-label="None" value="" />
+                                    <optgroup label="Undergraduate">
+                                        <option value="incoming_ug">Incoming Undergrad</option>
+                                        <option value="1A">1A</option>
+                                        <option value="1B">1B</option>
+                                        <option value="2A">2A</option>
+                                        <option value="2B">2B</option>
+                                        <option value="3A">3A</option>
+                                        <option value="3B">3B</option>
+                                        <option value="4A">4A</option>
+                                        <option value="4B">4B</option>
+                                        <option value="5A">5A</option>
+                                        <option value="5B">5B</option>
+                                    </optgroup>
+                                    <optgroup label="Graduate">
+                                        <option value="incoming_g">Incoming Grad</option>
+                                        <option value="G">Graduate</option>
+                                    </optgroup>
+                                </NativeSelect>
+                            <FormHelperText>Please select your current year of study</FormHelperText>
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel htmlFor="program-native-helper">Program</InputLabel>
+                                <NativeSelect
+                                    value={state.program}
+                                    onChange={handleDropDownChange}
+                                    inputProps={{
+                                        name: 'program',
+                                        id: 'program-native-helper',
+                                    }}
+                                    >
+                                    <option aria-label="None" value="" />
+                                    <optgroup label="Arts">
+                                        <option value="a">A</option>
+                                        <option value="b">B</option>
+                                        <option value="c">C</option>
+                                        <option value="d">D</option>
+                                        <option value="e">E</option>
+                                    </optgroup>
+                                    <optgroup label="Engineering">
+                                        <option value="f">F</option>
+                                        <option value="g">G</option>
+                                    </optgroup>
+                                </NativeSelect>
+                            <FormHelperText>Please select your program</FormHelperText>
+                        </FormControl>
+                        <Grid container spacing={10}>
+                            <Grid item xs={6}>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    className={classes.cancel}
+                                    onClick={handlePageChange}
+                                    xs={6}
+                                    >
+                                    Previous
+                                </Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    className={classes.submit}
+                                    onClick={handleSubmitClick}
+                                    xs={6}
+                                    >
+                                    Sign Up
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            );
+        }
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            padding: theme.spacing(10),
+            flexGrow: 1,
+        },
+        paper: {
+            margin: theme.spacing(5, 4),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            borderRadius: '35px',
+        },
+        avatar: {
+          margin: theme.spacing(1),
+          backgroundColor: '#ffbf00',
+        },
+        form: {
+          width: '100%', // Fix IE 11 issue.
+          marginTop: theme.spacing(2),
+        },
+        submit: {
+          margin: theme.spacing(3, 0, 2),
+          background: '#FFC100',
+          color: 'white',
+          borderRadius: 20,
+          height: '50px',
+          '&:hover': {
+            backgroundColor: 'white',
+            color: '#ffbf00',
+            },
+        },
+        cancel: {
+            margin: theme.spacing(3, 0, 2),
+            background: 'white',
+            color: '#ffbf00',
+            borderRadius: 20,
+            height: '50px',
+            '&:hover': {
+              backgroundColor: 'white',
+              color: '#FFC100',
+              },
+        },
+        img: {
+            margin: 'auto',
+            display: 'block',
+            maxWidth: '100%',
+            maxHeight: '100%',
+        },
+        link: {
+            color: "#AA8B2F",
+            "&:hover": {
+                color: "#E1B739",
+                textDecoration: "underline #ffbf00"
+            }
+        }
+    }))
+
+    const classes = useStyles();
+
+    return(
+        <Grid 
+            container 
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center" 
+            className={classes.root}
+            spacing={10}
+        >
+            <Grid item xs={12} sm={6} spacing={10}>
+                <h1>Welcome to WSource</h1>
+                <p>Log in to get your special recommendations!</p>
+                <img className={classes.img} src={welcome}/>
+                <Link href="https://storyset.com/people" variant="body2" color="textSecondary">People illustrations by Storyset</Link>
+            </Grid>
+            <Grid item xs={12} sm={6} component={Paper} elevation={6} spacing={10}>
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <SignUpForm />
                     <div className="alert alert-success mt-2" style={{display: state.successMessage ? 'block' : 'none' }} role="alert">
                         {state.successMessage}
                     </div>
